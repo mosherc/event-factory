7ed1d166afe234b1acf96c228928bbce9966cb48
Mosher, Colin <moldy912@gmail.com>
2018-04-11 16:31:39 -0400

    Fix pagination and move data initialization

--- src/app/event-factory/event-factory.component.ts
+++ src/app/event-factory/event-factory.component.ts
@@ -11,16 +11,17 @@ export class EventFactoryComponent implements OnInit {
   public events;
   public filteredEvents;
   public sortedEvents;
-  public pages;
-  public eventsPerPage = 500;
-  public currPage = 1;
-  public sorted = false;
-  public idModel;
-  public startTime;
-  public endTime;
-  public seqModel1;
-  public seqModel2;
-  public seqModel3;
+  public pages: number[];
+  public eventsPerPage: number = 500;
+  public currPage: number = 1;
+  public numPages: number;
+  public sorted: boolean = false;
+  public idModel: number;
+  public startTime: Date;
+  public endTime: Date;
+  public seqModel1: string;
+  public seqModel2: string;
+  public seqModel3: string;
 
   constructor(private _eventService: EventService) { }
 
@@ -33,18 +34,25 @@ export class EventFactoryComponent implements OnInit {
       data => {
         this.events = data;
         this.filteredEvents = data;
-        this.sortedEvents = [...this.events].sort((a, b) => a.created_at - b.created_at);
-        this.getPage(1);
-        let numPages = Math.ceil(this.events.length / this.eventsPerPage);
-        this.pages = Array.from(Array(numPages)).map((x, i) => i + 1);
       },
       err => console.error(err),
-      () => console.log('events loaded')
+      () => {
+        console.log('events loaded');
+        this.sortedEvents = [...this.events].sort((a, b) => a.created_at - b.created_at);
+        this.numPages = this.getNumPages();
+        this.getPage(1);
+        this.pages = Array.from(Array(this.numPages)).map((x, i) => i + 1);
+      }
     );
   }
 
+  getNumPages() {
+    return Math.ceil(this.events.length / this.eventsPerPage);
+  }
+
   getPage(n: number = this.currPage) {
-    const min = (n - 1) * this.eventsPerPage;
+    n = Math.max(1, Math.min(this.numPages, n));
+    const min = Math.max(n - 1) * this.eventsPerPage;
     const max = n * this.eventsPerPage;
     this.currPage = n;
     this.filteredEvents = this.sorted ? this.sortedEvents.slice(min, max) : this.events.slice(min, max);
